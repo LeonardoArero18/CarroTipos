23e747a04faa12c3941ab11172a76f55bc41e4d5
Leonardo Arero <65541707+arerobarbudovisk20@users.noreply.github.com>
2023-08-08 20:37:09 -0300

    Create class TipoCarroAutomatico.kt

---
+++ TipoCarroAutomatico.kt
@@ -0,0 +1,20 @@
+class TipoCarroAutomatico(
+    nome:String,
+    modelo:String,
+    ano:Int,
+    cor:String,
+    velocidadeAtual: Double
+
+):Carro(nome,modelo,ano,cor,velocidadeAtual) {
+
+    override fun detalhesCarro(): String {
+        return "Automatico, " +
+                "Freio ABS, " +
+                "Elétrico"
+
+    }
+
+
+
+
+}
