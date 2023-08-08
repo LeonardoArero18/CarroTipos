02bc042597ee6f4d772343413b16c479c366270f
Leonardo Arero <65541707+arerobarbudovisk20@users.noreply.github.com>
2023-08-08 20:24:22 -0300

    Create Carro.kt

---
+++ Carro.kt
@@ -0,0 +1,27 @@
+open class Carro(
+    var nome: String,
+    var modelo: String,
+    var ano: Int,
+    var cor: String,
+    var velocidadeAtual: Double
+){
+    open fun acelerarCarro():String{
+        if(velocidadeAtual == 0.0){
+            println("Carro Parado")
+        }
+
+        return "ACELERAR"
+    }
+
+    open fun freiarCarro():String{
+        if(velocidadeAtual >= 75.0){
+            println("Velocidade Máxima Atingida")
+        }
+        return "FREIAR"
+    }
+
+    open fun detalhesCarro():String{
+        return ""
+    }
+
+}
